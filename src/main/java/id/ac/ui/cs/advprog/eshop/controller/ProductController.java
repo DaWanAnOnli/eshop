42eb38315cdc141ef2a3671146e0232034a35d89
DaWanAnOnli <joel.rompis@gmail.com>
2024-02-19 19:12:35 +0700

    fix typo in ProductController.java and add @Repository annotation in CarRepository.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -94,7 +94,7 @@ class CarController extends ProductController{
         return "carList";
     }
 
-    @GetMapping("/editcar/{carId}")
+    @GetMapping("/editCar/{carId}")
     public String editCarPage(@PathVariable String carId, Model model){
         Car car = carservice.findById(carId);
         model.addAttribute("car", car);
